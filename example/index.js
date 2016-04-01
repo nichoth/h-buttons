3cd521b583be61676ff782634fbdfc16732c7c04
nichoth <nichoth@gmail.com>
2016-04-01 15:44:39 -0700

    style stuff

--- example/index.js
+++ example/index.js
@@ -1,9 +1,10 @@
 var delButton = require('../lib/delete')
 var h = require('bel').createElement
+var defaultStyle = require('../lib/delete.csjs')
 var csjs = require('csjs')
 
 var style = csjs`
-  .example extends ${delButton.style} {
+  .example extends ${defaultStyle['h-button-delete']} {
     width: 2em;
     height: 2em;
   }
